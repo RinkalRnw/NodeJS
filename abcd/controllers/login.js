7a60856ace7682c1c5aee5857aec9576caf56e96
Rinkalrnw <117502014+RinkalRnw@users.noreply.github.com>
2023-08-26 10:21:15 +0530

    jwt

--- abcd/controllers/login.js
+++ abcd/controllers/login.js
@@ -1,17 +1,24 @@
 const userModel = require('../models/userModel')
-
+const jwt = require('jsonwebtoken')
+const secretKey = "myfirstsecretkey"
 const checkLogin = async(req,res) => {
+
     let checkUser = await userModel.findOne({email:req.body.email, password:req.body.password});
     console.log("User login check...........")
     console.log(checkUser)
     if(checkUser){
-        res.cookie('loginemail',checkUser.name)
-        res.render('index')
+        jwt.sign({checkUser},secretKey,{expiresIn:'300s'},(err,token)=>{
+            res.json({token})
+        })
+        // res.cookie('loginemail',checkUser.name)
+        // res.render('index')
     } else {
         return res.send('Username or password incorrect')
     }
 }
-
+function verifyToken(req,res,next){
+    
+}
 module.exports = {
     checkLogin
 }
