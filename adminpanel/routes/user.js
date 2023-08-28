4f89790bfeafa965eb373765c41b04dab74503c3
Rinkalrnw <117502014+RinkalRnw@users.noreply.github.com>
2023-08-28 10:29:51 +0530

    Updated Code

--- adminpanel/routes/user.js
+++ adminpanel/routes/user.js
@@ -4,16 +4,7 @@ const bodyparser = body.urlencoded({extended: false})
 const passport = require("passport");
 const router = express.Router()
 
-<<<<<<< Updated upstream
-const {getDashboard, getForm, getPostData,checkUserData,checkLoginData,getOTP} = require("../controllers/userController")
-
-router.get('/forgotpassword',(req,res)=>{
-  res.render('forgetform')
-})
-router.post('/otp',bodyparser,getOTP)
-=======
 const {getDashboard, getForm, getPostData,checkUserData,checkLoginData} = require("../controllers/userController")
->>>>>>> Stashed changes
 
 router.get("/admin/data",getDashboard)
 router.get('/admin/form',getForm)
